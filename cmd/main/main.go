b9d98fb7aa6cb9be53105d42575afbf8a615c7a5
anas-infobell <anas@infobellit.com>
2023-10-27 15:44:49 +0530

    s

---
+++ cmd/main/main.go
@@ -0,0 +1,14 @@
+package main
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/anas-infobell/book_store/pkg/routes"
+)
+func main(){
+r := mux.NewRouter()
+routes.registerBookStoreRouters(r)
+http.Handle("/",r)
+log.Fatal(http.listenAndServe("localhost:9010",r))
+}
---
+++ pkg/routes/bookstore-route.go
@@ -0,0 +1,12 @@
+package routers
+import (
+"github.com/gorilla/mux"
+"github.com/anas-infobell/book_store/pkg/controllers"
+)
+var registerBookStoreRouters = func (router *mux.Router)  {
+	router.HandleFunc("/book/",controllers.CreateBook),Methods("POST")
+	router.HandleFunc("/book/",controllers.GetBook),Methods("GET")
+	router.HandleFunc("/book/{bookId}",controllers.GetBookById),Methods("GET")
+	router.HandleFunc("/book/{bookId}",controllers.UpdateBook),Methods("PUT")
+	router.HandleFunc("/book/{bookId}",controllers.DeleteBook),Methods("DELETE")
+}
